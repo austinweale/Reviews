10d6b48e0af850bedaf2331cda1b35db128a6b60
Austin Weale <austinweale@gmail.com>
2015-10-27 14:24:48 -0700

    simple parse stuff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+
+Parse.initialize("JBRs0gj6XUAiNnVz44x7QK8GIURYwIDVU00yRYIG", "6Z8kHIuoh1KVyIt09DG8xFm4hkDPPhk8Bykl6PTm");
+var Review = Parse.Object.extend('Review');
+
+
 window.onload = function(){
-	
+	$("#review-form").submit(submit);
 };
+
+function submit(){
+    var title = $("#title").val();
+    var review = $("#new-review").val();
+
+    var current = new Review;
+    
+    current.set("review", review);
+    current.save();
+}
