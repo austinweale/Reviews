af9c3caae834cd9d4cbb1d870b32caf7db66843d
studentuser <studentuser@D-173-250-180-64.dhcp4.washington.edu>
2015-10-28 13:19:09 -0700

    stars are somewhat working

--- js/main.js
+++ js/main.js
@@ -3,16 +3,36 @@ Parse.initialize("JBRs0gj6XUAiNnVz44x7QK8GIURYwIDVU00yRYIG", "6Z8kHIuoh1KVyIt09D
 var Review = Parse.Object.extend('Review');
 
 
+var starRating = -1;
+
+
 window.onload = function(){
 	$("#review-form").submit(submit);
+
+	var stars = document.querySelectorAll(".rating span");
+
+	for(var i = 0; i < stars.length; i++){
+		stars[i].onclick = changeRating;
+	}
+
 };
+
+function changeRating(){
+	starRating = parseInt(this.id);
+}
 
 function submit(){
     var title = $("#title").val();
     var review = $("#new-review").val();
 
-    var current = new Review;
-    
-    current.set("review", review);
-    current.save();
+
+    var currentReview = new Review;
+    currentReview.set("review", review);
+
+    currentReview.set("title", title);
+
+    currentReview.set("stars", starRating);
+
+    currentReview.save();
 }
+
