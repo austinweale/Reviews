6ec54d26ff5acd49260169eabfd3ec90c73a2412
studentuser <studentuser@studentusers-MacBook-Pro.local>
2015-10-29 17:07:52 -0700

    Final commit

--- js/main.js
+++ js/main.js
@@ -3,45 +3,47 @@ Parse.initialize("JBRs0gj6XUAiNnVz44x7QK8GIURYwIDVU00yRYIG", "6Z8kHIuoh1KVyIt09D
 var Review = Parse.Object.extend('Review');
 
 
-var itemCount;
-var totalStars;
+var itemCount; 
+var totalStars; //these are for getting the average star rating
 
 
+//setting up the submission, star rating, and populating the data
 window.onload = function(){
 	$("#form").submit(submit);
 
 	$("#rating").raty({path: "raty/lib/images/"});
-    $("#rating").onclick = changeRating;
+    
     getData();
 
 };
 
-function changeRating(){
-	var score = $("rating").raty('score');
-    alert(score);
-
-}
-
+//submits a review to the database
 function submit(){
-
     var title = $("#title").val();
     var review = $("#new-review").val();
+    var score = $("#rating").raty('score');
+    if(score == null){
+        alert("please  input a star rating");
+    }else if(title == ""){
+        alert("please enter a title for your review");
+    }else if(review == ""){
+        alert("please enter a review");
+    }else{
+        var currentReview = new Review;
+        currentReview.set("review", review);
 
+        currentReview.set("title", title);
 
-    var currentReview = new Review;
-    currentReview.set("review", review);
+        currentReview.set("stars", score);
 
-    currentReview.set("title", title);
+        currentReview.set("votes", 0);
 
-    currentReview.set("stars", $("#rating").raty('score'));
+        currentReview.save();
+    }
 
-    currentReview.set("votes", 0);
-
-    currentReview.save();
-
-    //getData();
 }
 
+//loads the review list
 function getData(){
     itemCount = 0;
     totalStars = 0;
@@ -56,6 +58,7 @@ function getData(){
     })
 }
 
+//builds the review area
 var buildList = function(data) {
     $('#review-area').html("");
     
@@ -66,6 +69,7 @@ var buildList = function(data) {
     $("#average").append(average);
 }
 
+//appends a review to the page
 var addItem = function(item, num) {
     
     var title = item.get("title");
@@ -90,12 +94,14 @@ var addItem = function(item, num) {
 
 
     $(div).addClass("line");
+    $(div).append(createUpvote(votes, objectID, num));
     $(div).append(h2);
     $(div).append(p);
-    $(div).append(createUpvote(votes, objectID, num));
+    
     $("#review-area").append(div);
 }
 
+//creates a star rating based on the passed-in star amount
 function createStarRating(stars){
     var rating = document.createElement("span");
     $(rating).addClass("star");
@@ -113,17 +119,19 @@ function createStarRating(stars){
 
 }
 
+//creates the upvote/downvote buttons and returns it in a div
 function createUpvote(currentCount, objectID, num){
     var div = document.createElement("div");
     $(div).addClass("upvote");
     
 
-
+    //holds the upvote number
     var count = document.createElement("span");
     $(count).addClass("count");
     $(count).html(currentCount);
     count.id = num + "";
 
+    //makes upvote arrow
     var up = document.createElement("a");
     $(up).addClass("upvote");
     up.onclick = function(){
@@ -131,19 +139,21 @@ function createUpvote(currentCount, objectID, num){
         incrementVote(1, objectID, num);
     };
     
-
+    //downvote arrow
     var down = document.createElement("a");
     $(down).addClass("downvote");
     down.onclick = function(){
         $(this).addClass("downvote-on")
         incrementVote(-1, objectID, num);
     };
+
     $(div).append(up);
     $(div).append(count);
     $(div).append(down);
     return div;
 }
 
+//increments the upvote count depending on the user's selection
 function incrementVote(vote, objectID, num){
     var query = new Parse.Query(Review);
 
